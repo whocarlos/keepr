093e95db22a20b4fd041930b78360b60520f88be
Carlos <regularlycarlos@gmail.com>
2023-12-26 10:23:52 -0600

    added form to login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,19 @@
 function Login(){
-    return <h1>Login page</h1>
+
+    
+    return (
+        <>
+        <h1>Login</h1>
+
+        <div>
+            <Form method="post" action="/login">
+                <input type="email" name="email" required />
+                <input type="password" name="password" required />
+                <button type="submit">login</button>
+            </Form>
+        </div>
+        </>
+    )
 }
 
 export default Login;
