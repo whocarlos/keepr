41520a79df6a6b0a04dd8bd903d2b1de7fea2ba2
Carlos <regularlycarlos@gmail.com>
2023-12-25 22:49:39 -0600

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <h1>I will ship this no matter what</h1>
+  )
+}
+
+export default App
