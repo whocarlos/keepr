b951922e7e3a328a90eb6a567f65a9ef413183c2
Carlos <regularlycarlos@gmail.com>
2023-12-27 11:44:46 -0600

    can toggle bookmark checkbox

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,18 @@ import { useAuth } from "../contexts/Auth";
 import { Navbar } from "../components/Navbar";
 import { Form } from "react-router-dom";
 import './Home.css'
+import { useState } from "react";
 
 function Home(){
     const session = useAuth();
     console.log(session.user)
+
+    const[isCheck, setIscheck] = useState(false);
+
+    function testCheckbox(){
+      console.log('u clicked the checkbox');
+      setIscheck(!isCheck);
+    }
     return (
         <div>
         <Navbar />
@@ -14,9 +22,23 @@ function Home(){
             <div className="create-container">
                 <div className='form-container'>
                     <Form>
-                        <div>
-                         <input type="text" name="title" placeholder="title"/>
-                         <input type="checkbox" name="bookmark" id="bookmark" />
+                        <div className="title-container">
+                        <input type="text" name="title" placeholder="title" id="title-input"/>
+                        <label htmlFor="bookmark" className="bookmark-icon">
+                          {
+                            isCheck ?
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+  <path fillRule="evenodd" d="M6.32 2.577a49.255 49.255 0 0 1 11.36 0c1.497.174 2.57 1.46 2.57 2.93V21a.75.75 0 0 1-1.085.67L12 18.089l-7.165 3.583A.75.75 0 0 1 3.75 21V5.507c0-1.47 1.073-2.756 2.57-2.93Z" clipRule="evenodd" />
+</svg>
+ :
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+<path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
+</svg>
+                          }
+
+<input type="checkbox" name="bookmark" id="bookmark" onClick={testCheckbox}/>
+
+                        </label>
                         </div>
                         <textarea name="content" id="" cols="30" rows="10"></textarea>
                         <div className='note-settings-icons-container'>
