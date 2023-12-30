a01ac1e0532d460fd5b1cea12521efa705dce4ee
Carlos <regularlycarlos@gmail.com>
2023-12-29 21:18:38 -0600

    moved input title to its own component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,18 +4,19 @@ import { NoteSettings } from "../components/NoteSettings";
 import { Form } from "react-router-dom";
 import './Home.css'
 import { useState, useRef } from "react";
+import { NoteTitleInput } from "../components/NoteTitleInput";
 
 
 function Home(){
     const session = useAuth();
     console.log(session.user)
 
-    const[isCheck, setIscheck] = useState(false);
+    // const[isCheck, setIscheck] = useState(false);
 
-    function testCheckbox(){
-      console.log('u clicked the checkbox');
-      setIscheck(!isCheck);
-    }
+    // function testCheckbox(){
+    //   console.log('u clicked the checkbox');
+    //   setIscheck(!isCheck);
+    // }
 
     const myRef = useRef(null);
 
@@ -37,24 +38,7 @@ function Home(){
             <div className="create-container">
                 <div className='form-container'>
                     <Form>
-                        <div className="title-container">
-                          <input type="text" id="title-input" placeholder="title" />
-                        <label htmlFor="bookmark" className="bookmark-icon">
-                          {
-                            isCheck ?
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-  <path fillRule="evenodd" d="M6.32 2.577a49.255 49.255 0 0 1 11.36 0c1.497.174 2.57 1.46 2.57 2.93V21a.75.75 0 0 1-1.085.67L12 18.089l-7.165 3.583A.75.75 0 0 1 3.75 21V5.507c0-1.47 1.073-2.756 2.57-2.93Z" clipRule="evenodd" />
-</svg>
- :
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-<path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
-</svg>
-                          }
-
-<input type="checkbox" name="bookmark" id="bookmark" onClick={testCheckbox}/>
-
-                        </label>
-                        </div>
+                      <NoteTitleInput />
                         <div  contentEditable='true' className="text-content" onInput={handleInput} ref={myRef} onKeyDown={handleKeyDow} plaintext-only='true'>
 
                         </div>
