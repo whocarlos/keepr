8db83de1310e3d34199f96432633efac2b603483
Carlos <regularlycarlos@gmail.com>
2024-01-04 15:34:29 -0600

    placeholder works

--- src/components/NoteTextInput.jsx
+++ src/components/NoteTextInput.jsx
@@ -4,18 +4,44 @@ export function NoteTextInput() {
 
     const [noteContent, setNoteContent] = useState('');
 
-    function handleInput(e){
-        let {innerHTML} = e.target;
+    function createNewDiv(parent) {
+        const newDiv = document.createElement("div");
+        const newContent = document.createTextNode('sup line');
+        newDiv.appendChild(newContent);
+        newDiv.classList.add('newline');
+        parent.appendChild(newDiv);
+
+    }
+
+    function handleInput(e) {
+        //console.log(e)
+        //e.preventDefault();
+        let { inputType } = e.nativeEvent;
+        console.log(inputType);
+
+        let { innerHTML } = e.target;
+        console.log(innerHTML)
+
+        let parent = e.target;
+        if (inputType === 'insertParagraph') {
+            console.log('this ran?')
+            //createNewDiv(parent);
+        }
+
+
         setNoteContent(innerHTML);
-        console.log('sup',noteContent);
+
+
+        //console.log('sup',noteContent);
     }
 
     return (
-        <div contentEditable='true'  
-        suppressContentEditableWarning='true' 
-        className='text-input' 
-        onInput={(e) => handleInput(e)}>
-            sup
+        <div contentEditable='true'
+            suppressContentEditableWarning='true'
+            className='text-input'
+            onInput={(e) => handleInput(e)}
+            placeholder="Add a note..."
+        >
         </div>
     )
 }
