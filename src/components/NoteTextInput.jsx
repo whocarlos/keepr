7ae5ce29cfd8e29d16c5042c3e252341afe598e8
Carlos <regularlycarlos@gmail.com>
2024-01-04 13:00:20 -0600

    here we go again

--- src/components/NoteTextInput.jsx
+++ src/components/NoteTextInput.jsx
@@ -4,8 +4,17 @@ export function NoteTextInput() {
 
     const [noteContent, setNoteContent] = useState('');
 
+    function handleInput(e){
+        let {innerHTML} = e.target;
+        setNoteContent(innerHTML);
+        console.log('sup',noteContent);
+    }
+
     return (
-        <div contentEditable='true'>
+        <div contentEditable='true'  
+        suppressContentEditableWarning='true' 
+        className='text-input' 
+        onInput={(e) => handleInput(e)}>
             sup
         </div>
     )
