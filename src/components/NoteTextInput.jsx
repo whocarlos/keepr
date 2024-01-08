419e3f805e3f35f60b6ccd039ace810da4a59583
Carlos <regularlycarlos@gmail.com>
2024-01-07 19:36:51 -0600

    let it quill

--- src/components/NoteTextInput.jsx
+++ src/components/NoteTextInput.jsx
@@ -1,61 +1,15 @@
 import { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
 export function NoteTextInput() {
 
     const [noteContent, setNoteContent] = useState('');
-
-    function handleInput(e){
-        console.log('this ran?');
-        
+    const quillModules = {
+        toolbar: false
     }
 
-    function checkForList(){
-        const selection = window.getSelection();
-        const range = selection.getRangeAt(0);
-        const line = range.startContainer.textContent.trim();
-
-        // Check for Ul
-        if(line[0] === '-'){
-            if(line[1] === ' ' && line[2] !== undefined){
-                console.log('ul mfer');
-                const referenceElem = range.startContainer.parentElement;
-                const referenceElemText = range.startContainer.parentElement.textContent;
-
-                referenceElem.outerHTML = `<ul><li>${referenceElemText}</li></ul>`;
-                
-                // newListElem.innerText = referenceElemCopy;
-
-                // const newnew = document.createElement('li');
-                // ul.appendChild(newnew);
-                //console.log('ul')
-                
-                
-
-               // referenceElem.append(ul);
-            }
-        }   
-
-        
-        //console.log('the line: ' ,line);
-    }
-
-    function handleKeyDown(e){
-        if(e.key === 'Enter'){
-            //console.log(selection);
-            //let {anchorNode, focusNode} = selection;
-            //console.log(anchorNode.parentElement, focusNode.parentElement);
-            checkForList();
-
-        }
-    }
-
-    return (
-        <div contentEditable='true'
-            suppressContentEditableWarning='true'
-            className='text-input'
-            placeholder="Add a note..."
-            onInput={handleInput}
-            onKeyDown={(e) => handleKeyDown(e)}
-        >
-        </div>
-    )
+    return(
+        <ReactQuill />
+    );
 }
