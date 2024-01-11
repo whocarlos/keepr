d30cdd7775cd76527f167baf87fe0a783c6e314f
Carlos <regularlycarlos@gmail.com>
2024-01-11 17:46:18 -0600

    back to basics (bye quill)

--- src/components/NoteTextInput.jsx
+++ src/components/NoteTextInput.jsx
@@ -1,48 +1,8 @@
 import { useEffect, useRef, useState } from 'react';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+
 
 export function NoteTextInput() {
-    const quillRef = useRef(null);
-    useEffect(() => {
-        if (quillRef.current) {
-            //console.log('mounted', quillRef.current.getEditor());
-            const quill = quillRef.current.getEditor();
-            //console.log(quill.container);
-
-            // quill.container.addEventListener('input', () => {
-            //     const listElements = quill.container.querySelectorAll('ol');
-            //     console.log(listElements);
-            //     if (listElements.length > 0) {
-            //         console.log('new list created');
-            //     }
-            // });
-
-            function handleKeyDown(e) {
-                
-                    const selection = window.getSelection();
-                    //console.log(selection);
-                    //let {anchorNode, focusNode} = selection;
-                    //console.log(anchorNode.parentElement, focusNode.parentElement);
-
-                    const range = selection.getRangeAt(0);
-                    const line = range.startContainer;
-                    console.log(line);
-                
-
-
-
-
-            }
-
-            quill.container.addEventListener('input', handleKeyDown);
-
-        }
-    }, []);
-
-
-
     return (
-        <ReactQuill placeholder='Add a note...' formats={['list']} ref={quillRef} />
-    );
+        <div contentEditable='true'></div>
+    )
 }
