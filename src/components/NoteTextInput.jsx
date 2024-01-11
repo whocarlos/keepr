e42677046cb03be2f5ff8b921ed389ce6cb83e9d
Carlos <regularlycarlos@gmail.com>
2024-01-10 20:04:46 -0600

    fixed placeholder color

--- src/components/NoteTextInput.jsx
+++ src/components/NoteTextInput.jsx
@@ -1,14 +1,37 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import ReactQuill, { Quill } from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
 export function NoteTextInput() {
+    const quillRef = useRef(null);
+    useEffect(() => {
+        if (quillRef.current) {
+            //console.log('mounted', quillRef.current.getEditor());
+            const quillEditor = quillRef.current.getEditor();
+            quillEditor.on('text-change', function (delta, oldDelta, source) {
 
-    const [noteContent, setNoteContent] = useState('');
-    
-  
+
+                //console.log(content);
+                //console.log(delta, oldDelta);
+                const ops = delta.ops;
+                //console.log(ops);
+
+
+                for (let i = 0; i < ops.length; i++) {
+                    if (ops[i].attributes && ops[i].attributes.list === 'ordered') {
+                        // console.log('list ol created');
+                        const currContent = quillEditor.getContents();
+                        console.log(currContent);
+                    }
+                }
+            })
+        }
+
+    }, []);
+
+
 
     return (
-        <ReactQuill placeholder='Add a note...'  formats={['list']}/>
+        <ReactQuill placeholder='Add a note...' formats={['list']} ref={quillRef} />
     );
 }
