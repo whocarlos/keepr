dadbf17a73a47bf97417f4bc5b7617b888ff073d
Carlos <regularlycarlos@gmail.com>
2024-02-01 14:49:07 -0600

    menu looks decent

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,36 +1,59 @@
 import { Form } from "react-router-dom";
 import { LightbulbIcon } from "./icons/LightbulbIcon";
+import { ReminderIcon } from "./icons/ReminderIcon";
+import { EditIcon } from "./icons/EditIcon";
+import { ArchiveIcon } from "./icons/ArchiveIcon";
+import { TrashIcon } from "./icons/TrashIcon";
 
-export function Menu(){
+export function Menu() {
     return (
         <div className="menu">
             <Form>
                 <fieldset>
                     <div className="menu-item">
-                        <LightbulbIcon />
-                        <input type="radio" id="notes-menu" name="page"/>
+                        <div className="menu-icon">
+                            <LightbulbIcon />
+                        </div>
+
+                        <input type="radio" id="notes-menu" name="page" />
                         <label htmlFor="notes-menu">Notes</label>
                     </div>
 
                     <div className="menu-item">
-                        <input type="radio" id="reminders-menu" name="page"/>
+                        <div className="menu-icon">
+                            <ReminderIcon />
+                        </div>
+
+                        <input type="radio" id="reminders-menu" name="page" />
                         <label htmlFor="reminders-menu">Reminders</label>
                     </div>
 
                     {/* To-do: render all the labels and make them accesible as a menu item (each) */}
 
                     <div className="menu-item">
-                        <input type="radio" id="editLabels-menu" name="page"/>
+                        <div className="menu-icon">
+                            <EditIcon />
+                        </div>
+
+                        <input type="radio" id="editLabels-menu" name="page" />
                         <label htmlFor="editLabels-menu">Edit labels</label>
                     </div>
 
                     <div className="menu-item">
-                        <input type="radio" id="archivedNotes-menu" name="page"/>
+                        <div className="menu-icon">
+                            <ArchiveIcon />
+                        </div>
+
+                        <input type="radio" id="archivedNotes-menu" name="page" />
                         <label htmlFor="archivedNotes-menu">Archived notes</label>
                     </div>
 
                     <div className="menu-item">
-                        <input type="radio" id="trash-menu" name="page"/>
+                        <div className="menu-icon">
+                            <TrashIcon />
+                        </div>
+
+                        <input type="radio" id="trash-menu" name="page" />
                         <label htmlFor="trash-menu">Trash</label>
                     </div>
                 </fieldset>
