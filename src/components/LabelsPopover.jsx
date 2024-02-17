e80fc7f510a88205d3e5da9481c4b38d0f9b416a
Carlos <regularlycarlos@gmail.com>
2024-02-17 10:41:38 -0600

    wip
    can search labels on the popover

--- src/components/LabelsPopover.jsx
+++ src/components/LabelsPopover.jsx
@@ -1,10 +1,37 @@
-export function LabelsPopover({labels}) {
+import { useState } from "react"
+
+export function LabelsPopover({labels, labelsPopoverRef}) {
+    
+    const [innerLabels, setInnerLabels] = useState(labels);
+
+    function handleLabelSearch(e){
+        const searchValue = e.target.value;
+
+        if(searchValue === ''){
+            setInnerLabels(labels);
+            labelsPopoverRef.current.style.height = '10rem !important';
+            labelsPopoverRef.current.style.overflowY = 'scroll';
+            return;
+        }
+        const filteredLabels = labels.filter((label) => {
+            return label.label_name.toLowerCase().includes(searchValue.toLowerCase())
+        })
+
+       
+
+        setInnerLabels(filteredLabels);
+        labelsPopoverRef.current.style.height = 'auto !important';
+        labelsPopoverRef.current.style.overflowY = 'hidden';
+    }
 
     return (
         <>
+        <div className="search-label-container">
         <p>Label note</p>
-        <input type="search" name="exact-label" id="" placeholder="Search label"/>
-        {labels.map((label) => {
+        <input type="search" name="exact-label" id="" placeholder="Search label" onInput={handleLabelSearch}/>
+        </div>
+        
+        {innerLabels.map((label) => {
             return (
                 <label htmlFor="" className="label-checkbox" key={label.label_id}>
                     <input type="checkbox" name="label" id={label.label_id} />
--- src/components/MorePopover.jsx
+++ src/components/MorePopover.jsx
@@ -1,21 +1,21 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { LabelsPopover } from "./LabelsPopover";
 
 export function MorePopover({labels}) {
 
 
     const [openLabelInput, setOpenLabelInput] = useState(false);
-    return (
-        <div className="popover">
-            {
-                openLabelInput ?
-                    <LabelsPopover labels={labels} /> :
-                    <>
-                        <div className="popover-item" onClick={() => setOpenLabelInput(true)}>Add label</div>
-                        <div className="popover-item">Add drawing</div>
-                    </>
-            }
+    const labelsPopoverRef = useRef(null);
 
-        </div>
+    return (
+        <>     {openLabelInput ?
+            <div className="labels-popover" ref={labelsPopoverRef}> <LabelsPopover labels={labels} labelsPopoverRef={labelsPopoverRef} /> </div>
+            :
+            <div className="popover">
+                <div className="popover-item" onClick={() => setOpenLabelInput(true)}>Add label</div>
+                <div className="popover-item">Add drawing</div>
+            </div>
+        }
+        </>
     )
 }
